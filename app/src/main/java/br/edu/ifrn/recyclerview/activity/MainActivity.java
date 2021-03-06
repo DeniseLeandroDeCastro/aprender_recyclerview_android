6cf75b0ae9f5c2d587c4f2d735555c83edb38d29
Denise <dmaria.natal@gmail.com>
2021-03-06 10:56:49 -0300

    aprender recyclerview android

---
+++ app/src/main/java/br/edu/ifrn/recyclerview/activity/MainActivity.java
@@ -0,0 +1,108 @@
+package br.edu.ifrn.recyclerview.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import adapter.Adapter;
+import br.edu.ifrn.recyclerview.R;
+import model.Filme;
+import recyclerview.RecyclerItemClickListener;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private List<Filme> listaFilmes = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        //Listagem de filmes
+        this.criarFilmes();
+
+        //Configurar adapter
+        Adapter adapter = new Adapter(listaFilmes);
+
+        //Configurar Recyclerview
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayout.VERTICAL));
+        recyclerView.setAdapter(adapter);
+
+        //evento de click
+        recyclerView.addOnItemTouchListener(
+                new RecyclerItemClickListener(
+                        getApplicationContext(),
+                        recyclerView,
+                        new RecyclerItemClickListener.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(View view, int position) {
+                                Filme filme = listaFilmes.get(position);
+                                Toast.makeText(
+                                        getApplicationContext(),
+                                        "Item pressionado: " + filme.getTituloFilme(),
+                                        Toast.LENGTH_SHORT
+                                ).show();
+                            }
+
+                            @Override
+                            public void onLongItemClick(View view, int position) {
+                                Filme filme = listaFilmes.get(position);
+                                Toast.makeText(
+                                        getApplicationContext(),
+                                        "Click longo: " + filme.getTituloFilme(),
+                                        Toast.LENGTH_LONG
+                                ).show();
+                            }
+
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                            }
+                        }
+                )
+        );
+    }
+    public void criarFilmes() {
+        Filme filme = new Filme("Homem Aranha - De volta ao lar", "Aventura", "2017");
+        this.listaFilmes.add(filme);
+
+        filme = new Filme("Mulher Maravilha", "Aventura", "2017");
+        this.listaFilmes.add(filme);
+
+        filme = new Filme("Liga da Justiça", "Aventura", "2017");
+        this.listaFilmes.add(filme);
+
+        filme = new Filme("It: A Coisa", "Terror", "2017");
+        this.listaFilmes.add(filme);
+
+        filme = new Filme("Pica-Pau: O Filme", "Comédia/Animação", "2017");
+        this.listaFilmes.add(filme);
+
+        filme = new Filme("A Bela e a Fera", "Romance", "2017");
+        this.listaFilmes.add(filme);
+
+        filme = new Filme("A Múmia", "Terror", "2017");
+        this.listaFilmes.add(filme);
+
+        filme = new Filme("Meu Malvado Favorito 3", "Comédia/Animação", "2017");
+        this.listaFilmes.add(filme);
+
+        filme = new Filme("Carros 3", "Comédia/Animação", "2017");
+        this.listaFilmes.add(filme);
+    }
+}
